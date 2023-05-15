c02c7e7583073813a7706259d1bac1b65d4fde98
kmssrv <fatalick@gmail.com>
2023-05-15 10:41:08 +0300

    commit

--- examples/add_correlation_rule.py
+++ examples/add_correlation_rule.py
@@ -1,12 +1,13 @@
 from pykumizer import Kuma
 import json
 
-with open('rule.json') as f:
+with open('detect_powershell_download.json') as f:
     rule = json.load(f)
 
 kuma = Kuma("kuma.kogorta.lab", "komissarov", "<password>")
 tenant_id = kuma.get_tenant_id_by_name('Main')
-correlation_rule_id = kuma.add_correlation_rule(rule)
+folder_id = kuma.get_folder_id_by_name('KOMISSAROV', 'correlationRule')
+correlation_rule_id = kuma.add_correlation_rule(rule, folder_id)
 correlator_id = kuma.get_correlator_id_by_name('[OOTB] Correlator', tenant_id)
 kuma.link_rule(correlator_id, correlation_rule_id)
 service_id = kuma.get_service_id_by_resouce_id(correlator_id)
--- pykumizer/main.py
+++ pykumizer/main.py
@@ -2,13 +2,15 @@ import requests
 import json
 import urllib.parse
 import urllib3
-
+import logging
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 class Kuma:
   def __init__(self, url, login, password):
+    logging.basicConfig(level=logging.INFO)
+    logging.info('Kuma init')
     self.login = login
     self.password = password
     self.url = "https://"+url+":7220"
@@ -27,19 +29,28 @@ class Kuma:
     self.session.headers.update({
       'X-XSRF-TOKEN': urllib.parse.unquote(self.session.cookies['XSRF-TOKEN'])
     })
+    logging.info('login: %s', self.login)
+    logging.info('password: %s', self.password)
+    logging.info('url: %s', url)
+    logging.info('response code: %s', response)
+    logging.info('response text: %s', response.text)
+
 
   def logout(self):
     url = self.url+"/api/logout"
     response = self.session.post(url, verify=False)
-    print(response)
-    print(response.text)
 
-  def add_correlation_rule(self, rule):
+  def add_correlation_rule(self, rule, folder_id):
+    logging.info('add_correlation_rule')
+    logging.info('folder_id: %s', folder_id)
     url = self.url+"/api/private/resources/correlationRule"
+    rule['folderID'] = folder_id
     payload = json.dumps(rule)
     response = self.session.post(url, data=payload)
-    print(response)
     correlation_rule_id = self.get_rule_id_by_name(rule['name'], rule['tenantID'])
+    logging.info('url: %s', url)
+    logging.info('response code: %s', response)
+    logging.info('response text: %s', response.text)
     return correlation_rule_id
 
   def get_tenant_id_by_name(self, name):
@@ -60,7 +71,6 @@ class Kuma:
     payload = json.dumps({"ids": [rule_id]})
     print(json.dumps(payload))
     response = self.session.delete(url, data=payload)
-    print(response.text)
 
   def get_correlator_id_by_name(self, name, tenant_id):
     url = self.url+"/api/private/resources/correlator?tenantID="+tenant_id
@@ -70,6 +80,7 @@ class Kuma:
         return correlator['id']
 
   def link_rule(self, correlator_id, correlation_rule_id):
+      logging.info('link_rule')
       url = self.url+"/api/private/resources/correlator/"+correlator_id
       correlation_rule_payload = self.get_correlation_rule_by_id(correlation_rule_id)
       correlator_data = self.get_correlator_by_id(correlator_id)
@@ -80,11 +91,14 @@ class Kuma:
 
       for cor_rul in correlator_data['payload']['rules']:
         if cor_rul['id'] == correlation_rule_id:
-          print('duplicate, skip')
+          logging.info('duplicate, skip')
           return
 
       correlator_data['payload']['rules'].append(correlation_rule_payload)
       response = self.session.put(url, data=json.dumps(correlator_data))
+      logging.info('url: %s', url)
+      logging.info('response code: %s', response)
+      logging.info('response text: %s', response.text)
 
   def get_resource_by_id(self, resource_id):
     url = self.url+"/api/private/resources/"
@@ -126,3 +140,15 @@ class Kuma:
     url = self.url+"/api/private/services/id/"+service_id+"/reload"
     payload = json.dumps({})
     response = self.session.post(url, data=payload)
+
+  def get_folder_id_by_name(self,folder_name, sub_kind):
+    logging.info('get_folder_id_by_name')
+    url = self.url+"/api/private/folders/?subKind="+sub_kind
+    response = self.session.get(url)
+    logging.info('url: %s', url)
+    logging.info('response code: %s', response)
+    logging.info('response text: %s', response.text)
+    for folder in response.json():
+      if folder['name'] == folder_name:
+        logging.info('ID of %s is %s', folder_name, folder['id'])
+        return folder['id']
