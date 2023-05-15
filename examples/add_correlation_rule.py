12ac0189272b019c454b85fbc2b3cc8dae74e515
kmssrv <fatalick@gmail.com>
2023-05-15 12:46:43 +0300

    commit

--- examples/add_correlation_rule.py
+++ examples/add_correlation_rule.py
@@ -4,9 +4,9 @@ import json
 with open('detect_powershell_download.json') as f:
     rule = json.load(f)
 
-kuma = Kuma("kuma.kogorta.lab", "komissarov", "<password>")
+kuma = Kuma("kuma.kogorta.lab", "admin", "<password>")
 tenant_id = kuma.get_tenant_id_by_name('Main')
-folder_id = kuma.get_folder_id_by_name('KOMISSAROV', 'correlationRule')
+folder_id = kuma.get_folder_id_by_name('OOTB', 'correlationRule')
 correlation_rule_id = kuma.add_correlation_rule(rule, folder_id)
 correlator_id = kuma.get_correlator_id_by_name('[OOTB] Correlator', tenant_id)
 kuma.link_rule(correlator_id, correlation_rule_id)
